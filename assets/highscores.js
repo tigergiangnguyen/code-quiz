32592c7389c14e1e372700247aa64a1f2392b457
tigergiangnguyen <tigernguyen5@yahoo.com>
2023-02-20 20:42:18 -0500

    modified function

--- assets/highscores.js
+++ assets/highscores.js
@@ -2,10 +2,7 @@ const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 highScoresList.innerHTML = highScores
-
-for (let i = 0; i < highScores.length; i++) {
-  let listItem = document.createElement("li");
-  listItem.className = "high-score";
-  listItem.textContent = highScores[i].name + " - " + highScores[i].score;
-  highScoresList.push(listItem);
-};
+  .map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  })
+  .join("");
